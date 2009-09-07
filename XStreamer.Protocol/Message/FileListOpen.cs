a75e1106434706f1a0e43284c812fa8d9bb11c6a
Brett Carswell <brett.carswell@gmail.com>
2009-09-07 10:48:36 +1000

    Messages to handle file operations

---
+++ XStreamer.Protocol/Message/FileListOpen.cs
@@ -0,0 +1,38 @@
+﻿namespace XStreamer.Protocol.Message
+{
+    public class FileListOpen : AbstractMessage
+    {
+        #region Overrides of AbstractMessage
+
+        /// <summary>
+        /// Gets the message type.
+        /// </summary>
+        /// <value>The message type.</value>
+        public override PacketType Type
+        {
+            get { return PacketType.FileListOpen; }
+        }
+
+        /// <summary>
+        /// Decodes the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public override void Decode(byte[] data)
+        {
+            DecodeHeader(data);
+        }
+
+        /// <summary>
+        /// Return the message encoded as bytes
+        /// </summary>
+        /// <returns>
+        /// An array of bytes containing the encoded message
+        /// </returns>
+        public override byte[] AsBytes()
+        {
+            return PacketData.CreateData(Type, MessageId, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ XStreamer.Protocol/Message/HandleMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XStreamer.Protocol.Message
+{
+    public class HandleMessage : AbstractMessage
+    {
+        #region Overrides of AbstractMessage
+
+        /// <summary>
+        /// Gets the message type.
+        /// </summary>
+        /// <value>The message type.</value>
+        public override PacketType Type
+        {
+            get { return PacketType.Handle; }
+        }
+
+        /// <summary>
+        /// Decodes the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public override void Decode(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return the message encoded as bytes
+        /// </summary>
+        /// <returns>
+        /// An array of bytes containing the encoded message
+        /// </returns>
+        public override byte[] AsBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
