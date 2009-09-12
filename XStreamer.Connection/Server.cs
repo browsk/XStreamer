eac79b4517a27599188b03f21ddf2dd06e7a0066
Brett Carswell <brett.carswell@gmail.com>
2009-09-13 08:53:57 +1000

    Added some possibly unnecesary cleanup code

--- XStreamer.Connection/Server.cs
+++ XStreamer.Connection/Server.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,6 +10,8 @@ namespace XStreamer.Connection
     public class Server : IDisposable
     {
         private TcpListener _listener;
+
+        private readonly IList<WeakReference> _connectedClients = new List<WeakReference>();
 
         public EventHandler<OnClientConnectEventArgs> OnClientConnect;
 
@@ -40,6 +43,24 @@ namespace XStreamer.Connection
         {
             TcpClient client = _listener.EndAcceptTcpClient(result);
 
+            // purge old clients
+            var closedClients = new List<WeakReference>();
+            foreach (var connectedClient in _connectedClients)
+            {
+                var weakClient = connectedClient.Target;
+                if (weakClient == null)
+                    closedClients.Add(connectedClient);
+            }
+
+            foreach (var closedClient in closedClients)
+            {
+                _connectedClients.Remove(closedClient);
+            }
+
+            // add new client
+            _connectedClients.Add(new WeakReference(client));
+
+
             // print a message
             byte[] message = Encoding.UTF8.GetBytes(Version);
             client.GetStream().Write(message, 0, message.Length);
@@ -49,9 +70,27 @@ namespace XStreamer.Connection
             _listener.BeginAcceptTcpClient(OnConnect, null);
         }
 
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _listener.Stop();
+
+                foreach (var weakClient in _connectedClients)
+                {
+                    var client = (TcpClient) weakClient.Target;
+                    if (client != null && client.Connected)
+                    {
+                        client.Close();
+                    }
+                }
+            }
+        }
+
         public void Dispose()
         {
-            _listener.Stop();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
